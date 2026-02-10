2234ba7cf4c366a602a6cdc18cd39c440c086331
renat001 <Otanerjacul@icloud.com>
2026-02-10 17:48:24 +0100

    lesson 1

---
+++ index.py
@@ -0,0 +1,16 @@
+name= ' renato '
+temperature= 42.5
+
+print(type(name))
+print(type(temperature))
+
+age= 16
+print("my age is", age)
+
+age=age+1
+print("my age now is", age)
+
+first_name= 'Renato'
+last_name= 'Lucaj'
+full_name= first_name + " " + last_name
+print(full_name)
