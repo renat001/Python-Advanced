1cacaaa83aaf01a1fcb4f50019776ee43c942554
renat001 <Otanerjacul@icloud.com>
2026-04-02 17:37:26 +0200

    lesson 13

---
+++ lesson 13/pandas_df.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = {'Name':['Egzon','Liron','Melina'],
+        'Age':['17','16','18'],
+        'City':['Fushe Kosove', 'Presheve','Prishtine']}
+
+df = pd.DataFrame(data)
+print(df)
