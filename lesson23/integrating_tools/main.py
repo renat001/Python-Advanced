5e5f01fb415a227556f5476c716fcf1d868245d9
renat001 <Otanerjacul@icloud.com>
2026-05-19 17:46:10 +0200

    lesson23

---
+++ lesson23/integrating_tools/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from models import Developer, Project
+
+app = FatAPI()
+
+@app.post("/developers/")
+def create_developer(developer: Developer):
+    return {"message": "Developer created successfully", "developer":developer}
+
+@app.post("/projects/")
+def create_project(project: Project):
+    return {"message": "Project created successfully", "project":project}
+
+@app.get("/projects/")
+def get_projects
+
+    sample_project = Project(
+        title="Sample Project",
+        description="This is a sample project",
+        languages=["Python", "Javascript"],
+        lead_developer=Developer(name="Dion Kabashi", expirience=5)
+    )
+    
